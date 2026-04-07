60a02c4d56a3535d83bd6f2098a270b98d45db2b
jaydenbarnescs-tech <jayden.barnes.cs@gmail.com>
2026-04-07 11:29:10 +0900

    feat: next.config

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/tools/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.notion.so https://*.notion.site https://notion.so",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
